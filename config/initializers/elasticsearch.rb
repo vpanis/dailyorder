bd3add97e3a575befd5f70b0724fe7dba3554403
Vincent Panis <vincent@come2link.com>
2017-07-17 18:06:21 +0200

    ok

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,3 @@
-ENV["ELASTICSEARCH_URL"] = "http://paas:2a0585d7a93f2ddd0e70ba90271b0cd9@ori-eu-west-1.searchly.com"
+ENV["ELASTICSEARCH_URL"] = "http://localhost:9200"
+
+# ENV["ELASTICSEARCH_URL"] = "http://paas:2a0585d7a93f2ddd0e70ba90271b0cd9@ori-eu-west-1.searchly.com"
