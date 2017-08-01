5d34581817f9d8802b95493b51e2fbb3ebcac0b1
Vincent Panis <vincent@come2link.com>
2017-08-01 16:38:25 +0200

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :smtp
   config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "http://www.dailyorder.co" }
+  config.action_mailer.default_url_options = { host: "http://www.daily-order.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
