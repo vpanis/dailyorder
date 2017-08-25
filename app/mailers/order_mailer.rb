dd039be3789c3a0c0193dc31551fc84b3236890f
Vincent Panis <vincent@come2link.com>
2017-08-25 11:41:16 +0200

    ok

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,8 @@
 class OrderMailer < ApplicationMailer
   def send_orders(supplier_id, orders_ids, document_id)
-    @supplier = Supplier.find(supplier_id.id)
-    @orders = orders_ids.map { |order_id| Order.find(order_id.id) }
-    @document = Document.find(document_id.id)
+    @supplier = Supplier.find(supplier_id)
+    @orders = orders_ids.map { |order_id| Order.find(order_id) }
+    @document = Document.find(document_id)
 
     attachments["rc_#{@document.created_at.strftime("%d_%m_%Y")}.pdf"] = WickedPdf.new.pdf_from_string(
       render_to_string(
