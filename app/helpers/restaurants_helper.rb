59ff33fc4cc9f4ca0fe82084594ea83cf54c5a9e
Vincent Panis <vincent@come2link.com>
2017-09-19 15:10:32 +0200

    ok

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,7 +1,7 @@
 module RestaurantsHelper
   def set_restaurant_photo(restaurant)
     avatar_url = restaurant.photo
-    avatar_url = cl_image_path(restaurant.photo.public_id, height: 100, width: 100, crop: :fill) if avatar_url.nil? && user.photo
+    avatar_url = cl_image_path(restaurant.photo.public_id, height: 100, width: 100, crop: :fill) if avatar_url.nil? && restaurant.photo
     avatar_url.nil? ? asset_path('default_restaurant.png') : avatar_url
   end
 
